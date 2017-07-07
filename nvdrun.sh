f0955d9ec04253ab325655a84c24910d3a5d7aff
khcs <khcs@users.noreply.github.com>
2017-07-07 17:53:42 -0400

    Update nvdrun.sh

--- nvdrun.sh
+++ nvdrun.sh
@@ -2,7 +2,7 @@
 # nvdrun.sh
 
 if [[ "$1" == "-h" ]] ; then
-    echo 'nvdrun.sh REGISTRY WORKGROUP FRAMEWORK VERSION WORKSPACE [DATASET_DIR: optional]'
+    echo 'nvdrun.sh REGISTRY WORKGROUP FRAMEWORK TAG WORKSPACE [DATASET_DIR: optional]'
     echo 'Example: nvdrun.sh XXXX.io tensorflow tensorflow 17.XX test [/tmp/datasets]'
     exit 0
 fi
@@ -23,7 +23,7 @@ if [[ $# -eq 2 ]] ; then
 fi
 
 if [[ $# -eq 3 ]] ; then
-    echo 'framework version arg missing! - 17.XX, latest, etc.'
+    echo 'framework tag arg missing! - 17.XX, latest, etc.'
     exit 0
 fi
 
@@ -35,7 +35,7 @@ fi
 registry=$1
 workgroup=$2
 framework=$3
-version=$4
+tag=$4
 workspace=$5
 
 if [[ $# -eq 5 ]] ; then
@@ -54,13 +54,13 @@ if [ ! -d ~/workspace/$workspace ]; then
     exit 0
 fi
 
-cp -r ~/workspace/$workspace /tmp/nvdrun/$framework
+rsync -rv --exclude=.git ~/workspace/$workspace /tmp/nvdrun/$framework
 
 if [[ $# -eq 5 ]] ; then
-    nvidia-docker run -v /tmp/nvdrun/$framework:/home/workspace --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $registry/$workgroup/$framework /bin/bash
+    nvidia-docker run -v /tmp/nvdrun/$framework:/home/workspace --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $registry/$workgroup/$framework:$tag /bin/bash
 else
-    nvidia-docker run -v /tmp/nvdrun/$framework:/home/workspace -v $dataset_dir:/datasets --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $registry/$workgroup/$framework /bin/bash
+    nvidia-docker run -v /tmp/nvdrun/$framework:/home/workspace -v $dataset_dir:/datasets --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $registry/$workgroup/$framework:$tag /bin/bash
 fi
 
-cp -r /tmp/nvdrun/$framework ~/nvdruns/$framework
+rsync -rv --exclude=.git /tmp/nvdrun/$framework ~/nvdruns/$framework
 rm ~/ipaddrs/"${framework}_docker.ipaddr"
