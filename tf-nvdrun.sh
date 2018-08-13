ee72afd3eb553741a2c8c7c1e4568b1de21180fa
Hoo Chang Shin <hshin@nvidia.com>
2018-08-12 20:51:08 -0700

    simplifying tf-nvdrun.sh

--- tf-nvdrun.sh
+++ tf-nvdrun.sh
@@ -23,7 +23,6 @@ else
     dataset_dir=$2
 fi
 
-mkdir -p /tmp/nvdrun/$framework
 mkdir -p ~/ipaddrs
 mkdir -p ~/nvdruns
 ip addr > ~/ipaddrs/"${framework}_docker.ipaddr"
@@ -34,8 +33,7 @@ if [ ! -d ~/workspace/$workspace ]; then
 fi
 
 if [[ $# -eq 5 ]] ; then
-    rsync -rv --exclude=.git ~/workspace/$workspace /tmp/nvdrun/$framework > /dev/null 2>&1
-    nvidia-docker run -v /tmp/nvdrun/$framework:/home/workspace --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $workgroup/$framework:$tag /bin/bash
+    nvidia-docker run -v ~/workspace/$workspace:/home/workspace --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $workgroup/$framework:$tag /bin/bash
 else
 
     if [ ! -d $dataset_dir ]; then
@@ -43,9 +41,6 @@ else
         exit 0
     fi
     
-    rsync -rv --exclude=.git ~/workspace/$workspace /tmp/nvdrun/$framework > /dev/null 2>&1
-    nvidia-docker run -v /tmp/nvdrun/$framework:/home/workspace -v $dataset_dir:/datasets --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $workgroup/$framework:$tag /bin/bash
+    nvidia-docker run -v ~/workspace/$workspace:/home/workspace -v $dataset_dir:/datasets --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -ti -p 8888:8888 -p 6006:6006 $workgroup/$framework:$tag /bin/bash
 fi
 
-rsync -rv --exclude=.git /tmp/nvdrun/$framework ~/nvdruns > /dev/null 2>&1
-rm ~/ipaddrs/"${framework}_docker.ipaddr"
